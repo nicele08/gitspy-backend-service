acc0cf974de7c7192378a5cc9317c2fdf13c398e
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-04-10 02:15:32 +0200

    feat: enable CORS for API routes

--- src/app/api/github/token/route.ts
+++ src/app/api/github/token/route.ts
@@ -3,9 +3,19 @@ import Keys from "@/utils/keys";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
+  const newHeaders = new Headers(req.headers);
+  newHeaders.set("Access-Control-Allow-Origin", "*");
+  newHeaders.set("Access-Control-Allow-Methods", "GET");
+  newHeaders.set("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
+  if (!code) {
+    return NextResponse.json(
+      { error: "No code provided" },
+      { status: 400, headers: newHeaders }
+    );
+  }
   try {
-    const { searchParams } = new URL(req.url);
-    const code = searchParams.get("code");
     const response = await axios.post(
       "https://github.com/login/oauth/access_token",
       {
@@ -22,14 +32,20 @@ export async function GET(req: Request) {
     );
 
     if (response.status === 200) {
-      return NextResponse.json(response.data, { status: 200 });
+      return NextResponse.json(response.data, {
+        status: 200,
+        headers: newHeaders,
+      });
     }
     return NextResponse.json(
       { error: "Authentication failed" },
-      { status: 401 }
+      { status: 401, headers: newHeaders }
     );
   } catch (err: any) {
     console.log(err);
-    return NextResponse.json({ error: err.message }, { status: 500 });
+    return NextResponse.json(
+      { error: err.message },
+      { status: 500, headers: newHeaders }
+    );
   }
 }
