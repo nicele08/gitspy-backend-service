77d0e39cb61956b8de71d150d51d72658e264577
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-04-11 22:10:04 +0200

    remove scope

--- src/app/api/github/login/route.ts
+++ src/app/api/github/login/route.ts
@@ -2,26 +2,8 @@ import Keys from "@/utils/keys";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const allScopes = [
-        'repo',
-        'admin:org',
-        'admin:public_key',
-        'admin:repo_hook',
-        'admin:org_hook',
-        'gist',
-        'notifications',
-        'user',
-        'delete_repo',
-        'write:discussion',
-        'read:discussion',
-        'write:packages',
-        'read:packages',
-        'delete:packages',
-        'admin:gpg_key',
-        'workflow',
-      ];
       const authUrl = `https://github.com/login/oauth/authorize?client_id=${
         Keys.GITHUB_CLIENT_ID
-      }&scope=${allScopes.join(' ')}`;
+      }`;
   return NextResponse.redirect(authUrl);
 }
